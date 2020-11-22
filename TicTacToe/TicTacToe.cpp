49b94e6abb29aa643cfbcf07963650e14531d187
VNTeneva19 <63718760+VNTeneva19@users.noreply.github.com>
2020-11-22 17:16:15 +0200

    Update choices

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -113,7 +113,7 @@ int main()
     if (choice == 2)
     {
        cout << "1. The game is played on a grid that's 3 squares by 3 squares." << endl;
-       cout << "2. You are X, your friend is O. Players take turns putting their marks in empty squares." << endl;
+       cout << "2. For example, you are X and your friend is O. Players take turns putting their marks in empty squares." << endl;
        cout << "3. The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner." << endl;
        cout << "4. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie." << endl;
     }
